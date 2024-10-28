a6721d6b923d869a7019abedbb33e0eae92b367b
Work <udecamtres@gmail.com>
2024-10-28 10:26:13 -0300

    Creacion de lista

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.noteapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList <String> listNote = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
-
-
     // Iniciando código
 
 
